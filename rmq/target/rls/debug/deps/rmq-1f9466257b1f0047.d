9772b523a8022cbc79a34812280ee5762e8a33eb
Thomas Mailund <mailund@birc.au.dk>
2022-01-25 13:09:53 +0100

    brute force solution

--- rmq/target/rls/debug/deps/rmq-1f9466257b1f0047.d
+++
@@ -1,5 +0,0 @@
-/Users/mailund/Projects/rustRMQ/rmq/target/rls/debug/deps/rmq-1f9466257b1f0047.rmeta: src/lib.rs
-
-/Users/mailund/Projects/rustRMQ/rmq/target/rls/debug/deps/rmq-1f9466257b1f0047.d: src/lib.rs
-
-src/lib.rs:
--- rmq/target/rls/debug/deps/rmq-9c1593e33537fdf1.d
+++
@@ -1,5 +0,0 @@
-/Users/mailund/Projects/rustRMQ/rmq/target/rls/debug/deps/rmq-9c1593e33537fdf1.rmeta: src/lib.rs
-
-/Users/mailund/Projects/rustRMQ/rmq/target/rls/debug/deps/rmq-9c1593e33537fdf1.d: src/lib.rs
-
-src/lib.rs:
